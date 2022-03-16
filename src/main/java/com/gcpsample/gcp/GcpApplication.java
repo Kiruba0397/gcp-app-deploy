33ef929af018ac8997a5a201568c069b164db1ed
Kiruba0397 <56583046+celinwincy0310@users.noreply.github.com>
2022-03-16 15:00:24 +0530

    Gcp project

---
+++ src/main/java/com/gcpsample/gcp/GcpApplication.java
@@ -0,0 +1,23 @@
+package com.gcpsample.gcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpApplication.class, args);
+	}
+
+	
+	   @GetMapping("/")
+	   public String hello() {
+	     return "Welcome to Google cloud platform!";
+
+
+}
+}
